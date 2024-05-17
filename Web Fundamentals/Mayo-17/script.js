1f3b3bb9454f1d6a2f495214157782a3787e25ed
Nestor Eduardo Ribero Vargas <nestor_05ribero@hotmail.com>
2024-05-17 13:02:47 -0500

    update

---
+++ Web Fundamentals/Mayo-17/script.js
@@ -0,0 +1,38 @@
+
+let nombreContacto1 = "Nestor";
+let numeroContacto1 = "123489598";
+
+let nombreContacto2 = "Eduardo";
+let numeroContacto2 = "123489598";
+
+let nombreContacto3 = "Ribero";
+let numeroContacto3 = "123489598";
+
+
+let canciones = ["Beat it", "Thriller", "Smooth criminal"];
+
+
+console.log(canciones[2]);
+
+let favoritos = [["1", "7", "8"], canciones, ["Francia", "China", "USA"]];
+
+console.log(favoritos[2][1]);
+
+
+let contacto = {
+    nombre: "Nestor",
+    apellido: "Ribero",
+    edad: 26,
+    verificado: true,
+    idiomas:["Español","Francés","Inglés"]
+}
+
+console.log(contacto.nombre);
+console.log(contacto["apellido"]);
+console.log(contacto.idiomas[1]);
+
+
+
+
+
+
